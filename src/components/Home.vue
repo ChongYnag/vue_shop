9751f232951c1d10ee9b5d1ef8cd043af4e05b6f
jiangchongyang <jiangchongyang@onething.net>
2020-05-14 17:57:26 +0800

    fix

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    home组件
+    <el-button @click="logout" type="info">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login, auth: false },
+  { path: '/home', component: Home, auth: true }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 全局路由守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next();
+  const isToken = window.sessionStorage.getItem("token");
+  if (!isToken) return next('/login');
+  next()
 })
 
 export default router
